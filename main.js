1f02d4dba8d79028fd4e0e9560dc9e2d45527b2c
ersidimatranxhi <ersidi.matranxhi1@gmail.com>
2022-03-22 17:54:53 +0100

    quasi finito esercizio

--- main.js
+++ main.js
@@ -4,21 +4,30 @@ console.log(anni);
 let km = prompt("inserisci il numero dei km");
 console.log(km);
 
-let prezzoTragitto = km*0.21;
+let prezzoTragitto=km*0.21;
 console.log(prezzoTragitto); 
 
-let prezzoFinale;
-let sconto;
+let scontoPerMinori=prezzoTragitto*0.2
+console.log(scontoPerMinori);
 
+let scontoPerOver65=prezzoTragitto*0.4
+console.log(scontoPerOver65);
+
+let prezzoFinale;
 
 if (anni <18){
-    alert("Non hai compiuto 18 anni")
+    alert("Non hai compiuto 18 anni");
+    prezzoFinale=prezzoTragitto-scontoPerMinori;
 }
 
 else if (anni >65){
-    alert("Non hai compiuto 65 anni")
+    alert("Non hai compiuto 65 anni");
+    prezzoFinale=prezzoTragitto-scontoPerOver65;
 }
 
 else {
-    alert("Non hai diritto allo sonto")
+    alert("Non hai diritto allo sonto");
+    prezzoFinale=prezzoTragitto;
 }
+
+console.log("questo e il prezzo dedicato a te"+prezzoFinale) 
