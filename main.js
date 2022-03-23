ead142187bc0fc08a996f2d36a5c438f8e85a7d5
ersidimatranxhi <ersidi.matranxhi1@gmail.com>
2022-03-23 11:04:55 +0100

    esercizio terminato con i bonus

--- main.js
+++ main.js
@@ -1,33 +1,38 @@
 let anni = prompt("inscerisci eta");
+anni=parseFloat(anni);
 console.log(anni);
 
-let km = prompt("inserisci il numero dei km");
-console.log(km);
+let chilometri = prompt("inserisci il numero dei chilometri");
+chilometri=parseFloat(chilometri);
+console.log(chilometri);
 
-let prezzoTragitto=km*0.21;
-console.log(prezzoTragitto); 
-
-let scontoPerMinori=prezzoTragitto*0.2
-console.log(scontoPerMinori);
-
-let scontoPerOver65=prezzoTragitto*0.4
-console.log(scontoPerOver65);
+let prezzoSenzaSconto=chilometri*0.21;
+console.log(prezzoSenzaSconto); 
 
 let prezzoFinale;
 
 if (anni <18){
     alert("Non hai compiuto 18 anni");
-    prezzoFinale=prezzoTragitto-scontoPerMinori;
-}
-
-else if (anni >65){
+    
+    //CALCOLO DELLO SCONTO PER UNDER 18
+    let scontoPerMinori=prezzoSenzaSconto*0.2
+    console.log(scontoPerMinori);
+    prezzoFinale=prezzoSenzaSconto-scontoPerMinori;
+} else if (anni >65){
     alert("Non hai compiuto 65 anni");
-    prezzoFinale=prezzoTragitto-scontoPerOver65;
-}
+    //CALCOLO DELLO CONTO SOPRA 65 ANNI
 
-else {
+    let scontoPerOver65=prezzoSenzaSconto*0.4
+    console.log(scontoPerOver65);
+    prezzoFinale=prezzoSenzaSconto-scontoPerOver65;
+} else {
     alert("Non hai diritto allo sonto");
-    prezzoFinale=prezzoTragitto;
+    prezzoFinale=prezzoSenzaSconto;
 }
 
-console.log("questo e il prezzo dedicato a te"+prezzoFinale) 
+
+let prezzoFinaleDueDecimali=prezzoFinale.toFixed(2); 
+
+console.log("questo e il prezzo dedicato a te"+prezzoFinaleDueDecimali); 
+
+document.getElementById("costo").innerHTML=prezzoFinaleDueDecimali;
